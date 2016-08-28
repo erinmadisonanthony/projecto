3cccdce8d708fb7bffc05dca5aff31409ba9ad3d
Erin Anthony <erinmadisonanthony@gmail.com>
2016-08-28 22:11:51 +0000

    enable toggling of class completion

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+
   describe "tasks#index" do
     it "should list the tasks in the database" do
       task1 = FactoryGirl.create(:task)
@@ -12,4 +13,15 @@ RSpec.describe TasksController, type: :controller do
       expect(response_value.count).to eq(2)
     end
   end
+
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
+      task = FactoryGirl.create(:task, done: false)
+      put :update, id: task.id, task: { done: true }
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
+    end
+  end
+
 end
